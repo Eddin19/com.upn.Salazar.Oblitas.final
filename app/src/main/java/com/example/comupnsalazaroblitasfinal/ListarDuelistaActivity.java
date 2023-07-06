112433130f7822141c67e7558da63aac76846e0f
Eddin19 <salazaroblitas@gmail.com>
2023-07-06 10:31:19 -0500

    version7

--- app/src/main/java/com/example/comupnsalazaroblitasfinal/ListarDuelistaActivity.java
+++ app/src/main/java/com/example/comupnsalazaroblitasfinal/ListarDuelistaActivity.java
@@ -1,8 +1,13 @@
 package com.example.comupnsalazaroblitasfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ListarDuelistaActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class ListarDuelistaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_duelista);
+
+        RecyclerView rvLista = findViewById(R.id.rvListaDuelista);
+        rvLista.setLayoutManager(new LinearLayoutManager(this));
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://6477a0a19233e82dd53bf49a.mockapi.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        
+
+
+
     }
 }
